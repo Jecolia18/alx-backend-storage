c2923082a4be90ab992f73da5e19da37b42b8dfb
Jecolia <jecoliatieleo@gmail.com>
2024-12-10 03:17:48 -0800

    this

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+--script to create a table
+--id, name, email
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY  AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
